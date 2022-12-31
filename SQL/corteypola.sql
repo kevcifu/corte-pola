961f9d47966056adc67813fa65a425bdc0f9d962
kevcifu <kevins_cifuentes@soy.sena.edu.co>
2022-12-31 12:28:52 -0500

    SQL error fixed

--- SQL/corteypola.sql
+++ SQL/corteypola.sql
@@ -7,6 +7,9 @@
 -- Versión del servidor: 10.4.24-MariaDB
 -- Versión de PHP: 8.1.6
 
+CREATE DATABASE corteypola;
+USE corteypola;
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
